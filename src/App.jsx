5494448cbeda8ac115e386bed9e8b3a7be12f73f
SantiagoVillamilC <santyago4993@gmail.com>
2024-09-12 21:03:09 -0500

    entrance made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
+
 import CsvLoader from './components/CsvLoader';
 import BarChart from './components/BarChart';
 import LineChart from './components/LineChart';
@@ -10,15 +12,27 @@ import PieChart from './components/PieChart';
 import Histogram from './components/Histogram';
 import RadarChart from './components/RadarChart';
 
-const App = () => {
-  const [csvData, setCsvData] = useState([]);
+const texts = ["+30.000 registros", "# gráficas", "Datos actualizados", "Análisis en curso"];
 
-  console.log('csvData en App.jsx:', csvData); // Imprimir datos en consola para verificar que se cargan
+const App = () => {
+  //const [csvData, setCsvData] = useState([]);
+
+  //console.log('csvData en App.jsx:', csvData); // Imprimir datos en consola para verificar que se cargan
+
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
+
+  useEffect(() => {
+    const animation = animate(count, 20000, { duration: 10 });
+
+    return animation.stop;
+  }, []);
+
 
   return (
     <div>
       <header>
-      <h4>Estate View - Estadistica</h4>
+        <h4>Estate View - Estadistica</h4>
         <div>
           <u>
             <ol>¿Que es?</ol>
@@ -27,25 +41,34 @@ const App = () => {
           </u>
         </div>
       </header>
-      <div>
-        <p>+30.000 registros</p>
-        <p>Akguna frase que explique el sitio brevemente</p>
-        <button>Descargar archivo original</button>
-        <button>Ver tabla de datos</button>
+      <div className='containerIntro'>
+        <div className='containerIntroTitle'>
+          <p className='textIntro'>Análisis con más de</p>
+          <p className='textIntro'><motion.span className='titleIntro'>{rounded}</motion.span> datos inmobiliarios</p>
+        </div>
+        <div className='containerIntroInfo'>
+          <p>Descubre cómo la distancia al centro de la ciudad de Melbourne impacta directamente en los precios del mercado inmobiliario a través de nuestra plataforma.</p>
+          <div>
+            <button>Descargar archivo original (.csv)</button>
+            <button>Ver tabla de datos</button>
+          </div>
+        </div>
+
+
       </div>
       {/* <div>
       <CsvLoader setCsvData={setCsvData} />
       <ScatterPlot csvData={csvData} />
       </div> */}
-      <CsvLoader/>
+      <CsvLoader />
       <h1>Visor de Datos Inmobiliarios</h1>
-      <BarChart/>
-      <LineChart/>
+      <BarChart />
+      <LineChart />
       <h1>Principal:</h1>
-      <ScatterPlot/>
-      <PieChart/>
-      <Histogram/>
-      <RadarChart/>
+      <ScatterPlot />
+      <PieChart />
+      <Histogram />
+      <RadarChart />
       {/* <BoxPlotChart/> */}
     </div>
   );
