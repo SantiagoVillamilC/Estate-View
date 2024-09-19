d021c12bab3f231624163c25c88f594b604bdcf8
SantiagoVillamilC <santyago4993@gmail.com>
2024-09-19 17:24:48 -0500

    readme update

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# React + Vite
+# Proyecto de Análisis de Mercado Inmobiliario en Melbourne
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**https://main--estadistica-stateview.netlify.app**
 
-Currently, two official plugins are available:
+Este proyecto es una aplicación web interactiva que permite la visualización y análisis de datos del mercado inmobiliario en Melbourne. La aplicación está construida con **React** y utiliza **Vite** para una configuración rápida y ligera. Los datos se procesan a partir de un archivo CSV y se muestran a través de tablas y gráficos interactivos.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Características Principales
+
+- **Carga y procesamiento de archivos CSV**: Los usuarios pueden cargar archivos de datos inmobiliarios en formato CSV, que luego se procesan y muestran en la interfaz.
+- **Visualización de datos**: Incluye gráficos interactivos que permiten observar la relación entre la distancia de las propiedades al centro de la ciudad (CBD) y sus precios.
+- **Tabla de frecuencia**: Los datos están organizados en una tabla de frecuencia que muestra el precio promedio de las propiedades en diferentes rangos de distancia al CBD.
+- **Gráficos de dispersión**: Permite visualizar la distribución de los precios de las propiedades en función de su distancia al CBD.
+- **Cálculos estadísticos**: Se incluyen medidas de variación como desviación estándar y precio promedio por cada rango de distancia.
+- **Frontend dinámico**: Desarrollado con **React** y **Vite** para asegurar un rendimiento óptimo y tiempos de carga rápidos.
+
+## Instalación y Ejecución
+
+- npm install
+
+-npm run dev
+
+## Dependencias
+
+- **React**
+- **Vite**
+- **PapaParse**
+- **React-Charts**
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -71,18 +71,17 @@ const App = () => {
           {/* <h1>Principal:</h1> */}
           <ScatterPlot />
           <p>Este gráfico muestra cómo el precio de las propiedades varía según su distancia al centro de Melbourne y cada punto en el gráfico representa una propiedad, con la distancia al CBD en el eje horizontal (X) y el precio en el eje vertical (Y).
+            <br />
+            <br />
 
-            Interpretación:
-
-            Propiedades cercanas al CBD (0-5 km): Generalmente, estos puntos están en la parte superior del gráfico, indicando que los precios son más altos en áreas cercanas al centro de la ciudad.
-            Propiedades a mediana distancia (5-20 km): Los precios suelen ser más bajos en comparación con las propiedades cercanas al CBD, aunque todavía pueden ser elevados dependiendo del barrio.
-            Propiedades más alejadas (20-30 km y más de 30 km): Los precios tienden a ser significativamente más bajos a medida que te alejas del centro de la ciudad. Estas áreas suelen ser más accesibles para quienes buscan opciones más económicas.
-            Conclusiones:
-
-            Tendencia General: Los precios de las propiedades disminuyen a medida que la distancia al centro de Melbourne aumenta. Esto refleja la alta demanda y el valor asociado con las ubicaciones centrales.
-            Zonas de Alta Demanda: Si estás buscando una propiedad cerca del centro de Melbourne, prepárate para precios más altos. Alternativamente, si el presupuesto es una preocupación, considerar áreas más alejadas puede ofrecer mejores ofertas.
-            Este gráfico te permite visualizar de manera clara cómo la proximidad al centro urbano afecta el precio de las propiedades, ayudándote a tomar decisiones informadas sobre la compra o inversión en bienes raíces.</p>
+            <strong>Propiedades cercanas al CBD (0-5 km):</strong> Generalmente, estos puntos están en la parte superior del gráfico, indicando que los precios son más altos en áreas cercanas al centro de la ciudad. <br />
+            <strong>Propiedades a mediana distancia (5-20 km):</strong> Los precios suelen ser más bajos en comparación con las propiedades cercanas al CBD, aunque todavía pueden ser elevados dependiendo del barrio. <br />
+            <strong>Propiedades más alejadas (20-30 km y más de 30 km):</strong> Los precios tienden a ser significativamente más bajos a medida que te alejas del centro de la ciudad por lo que estas áreas suelen ser más accesibles y por supuesto son opciones más económicas. <br />
+            <br />
+            Esto nos indica que los precios de las propiedades disminuyen a medida que la distancia al centro de Melbourne aumenta, reflejando la alta demanda y el valor asociado con las ubicaciones centrales;
+            Concluyendo que buscando una propiedad cerca del centro de Melbourne, es facil encontrar precios más altos, en cambio considerar áreas más alejadas puede ofrecer mejores ofertas, por supuesto que sacrificando algunas facilidades que ofrece el centro</p>
         </article>
+        <p></p>
         <article className='frecuencyTableMain'>
           <FrequencyTable />
         </article>
